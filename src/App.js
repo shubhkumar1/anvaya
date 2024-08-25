b9faa4d632bb46dfda88aa15e1bc10c66e7aa73e
shubhkumar1 <shubhamkumar4664@hotmail.com>
2024-08-26 01:56:58 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,189 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Editor from '@monaco-editor/react';
+import Navbar from './Navbar';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [userCode, setUserCode] = useState(``);
+	const [userLang, setUserLang] = useState(`python3`);
+	const [userTheme, setUserTheme] = useState(`vs-dark`);
+	const [fontSize, setFontSize] = useState(`20`);
+	const [userInput, setUserInput] = useState(``);
+	const [userOutput, setUserOutput] = useState(``);
+	const [loading, setLoading] = useState("false");
+
+	const options = {
+		fontSize: fontSize
+	}
+
+	async function compile() {
+
+		setLoading("true");
+
+		let headersList = {
+			"Content-Type": "application/json"
+		}
+		let version;
+		if (userLang === "python3") {
+			version = "3.10.0"
+		}
+		else if (userLang === "java") {
+			version = "15.0.2"
+		}
+		else if (userLang === "c" || userLang === "cpp") {
+			version = "10.2.0"
+		}
+
+		let bodyContent = JSON.stringify({
+			"language": userLang,
+			"version": version,
+			"files": [
+				{
+					"name": "main",
+					"content": userCode
+				}
+			],
+			"stdin": userInput
+		});
+
+		let response = await fetch("https://emkc.org/api/v2/piston/execute", {
+			method: "POST",
+			body: bodyContent,
+			headers: headersList,
+		});
+		try {
+			let data = await response.json();
+			console.log(data);
+			setUserOutput(data.run.stdout || data.run.stderr);
+			setLoading("false");
+		}
+		catch (error) {
+			console.log(error);
+			setUserOutput("Error: " + (error.response ? error.response.data.error : error.message));
+			setLoading("false");
+		}
+
+		// const options = {
+		// 	url: "https://emkc.org/api/v2/piston/execute",
+		// 	method: 'POST',
+		// 	headers: {
+		// 		"Accept": "*/*",
+		// 		"Authorization": "a3eebabdc5c6260cd9d73d36aa12a98c",
+		// 		"Content-Type": "application/json"
+		// 	},
+		// 	body: {
+		// 		"language": userLang,
+		// "version": version,
+		// "files": [
+		// 	{
+		// 		"name": "main",
+		// 		"content": userCode
+		// 	}
+		// ],
+		// "stdin": userInput
+		// 	}
+		// };
+
+		// try {
+		// 	const response = await axios.request(options);
+		// 	
+		// } catch (error) {
+		// 	console.error(error);
+		// }
+	}
+
+	let bgColor;
+	if (userTheme === "vs-dark") {
+		bgColor = "#1e1e1e";
+	}
+
+
+	return (
+		<div style={{
+			backgroundColor: bgColor
+		}}>
+			<Navbar
+				userLang={userLang} setUserLang={setUserLang}
+				userTheme={userTheme} setUserTheme={setUserTheme}
+				fontSize={fontSize} setFontSize={setFontSize} compile={compile}
+			/>
+			<div style={{
+				display: "flex",
+				justifyContent: "space-evenly"
+			}}>
+				<Editor
+					options={options}
+					height="calc(100vh - 50px)"
+					width="60vw"
+					theme={userTheme}
+					language={userLang}
+					defaultLanguage='python'
+					defaultValue="// Enter your code here"
+
+					onChange={(value) => {
+						setUserCode(value)
+					}}
+				/>
+
+
+				<div style={{
+					backgroundColor: "#1e1e1e",
+					padding: "5px 10px",
+					borderRadius: "10px"
+				}}>
+
+					<div style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						color: 'white',
+						width: "100%"
+					}}>
+						<input type='text' placeholder='Input' style={{
+							width: "100%",
+							height: "50px",
+							borderRadius: "10px",
+							fontSize: "1.5rem"
+						}} onChange={(e) => setUserInput(e.target.value)} />
+						<h2>Output : </h2>
+					</div>
+
+					{
+						loading === "true" ? (
+							<div style={{
+								width: "35vw",
+								margin: "auto",
+								display: 'flex',
+								justifyContent: 'center',
+								flexDirection: 'column',
+								alignItems: 'center',
+								color: "white"
+							}}>
+								<span class="loader" />
+								<h3>Loading...</h3>
+							</div>
+						) : (
+							<pre style={{
+								fontSize: "1.5rem",
+								fontFamily: "Nanum Gothic Coding, monospace",
+								fontWeight: "400",
+								width: "35vw",
+								margin: "0",
+								color: 'white',
+								overflow: "auto"
+							}}>{userOutput}</pre>
+						)
+					}
+
+				</div>
+			</div>
+
+		</div>
+	);
+
+
+
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Select from 'react-select';
+import { useMediaQuery } from 'react-responsive';
+
+
+const Navbar = ({ userLang, setUserLang, userTheme, setUserTheme, fontSize, setFontSize, compile }) => {
+
+    const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+    // const isBigScreen = useMediaQuery({ minWidth: 1824 })
+    // const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+    // const isPortrait = useMediaQuery({ orientation: 'portrait' })
+    // const isRetina = useMediaQuery({ minResolution: '2dppx' })
+
+    const languages = [
+        { value: "c", label: "C" },
+        { value: "cpp", label: "C++" },
+        { value: "python3", label: "Python" },
+        { value: "java", label: "Java" },
+    ];
+
+    const themes = [
+        { value: "vs-dark", label: "Dark" },
+        { value: "light", label: "Light" },
+    ];
+
+    let fontColor;
+    if (userTheme === "vs-dark") {
+        fontColor = "white";
+    }
+
+    return (
+        <div style={{
+            padding: '10px 0'
+        }}>
+            {isDesktopOrLaptop && (
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly"
+            }}>
+                <h1 style={{
+                    color: 'grey',
+                    fontFamily: "DM Serif Text",
+                    fontWeight: 'bolder',
+                    margin: 0
+                }}>anvaya</h1>
+
+                <Select options={languages} value={userLang} onChange={(e) => setUserLang(e.value)} placeholder={userLang} />
+                <Select options={themes} value={userTheme} onChange={(e) => setUserTheme(e.value)} placeholder={userTheme} />
+
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    width: "200px",
+                    color: fontColor
+                }}>
+                    <label>Font Size</label>
+                    <input type='range' min="18" max="30" value={fontSize} step="2" onChange={(e) => { setFontSize(e.target.value) }} />
+                </div>
+                <button onClick={compile} style={{
+                    width: "10rem",
+                    height: "2.5rem",
+                    borderRadius: "10px"
+                }}>Run</button>
+            </div> )}
+
+        </div>
+    )
+}
+
+export default Navbar;
